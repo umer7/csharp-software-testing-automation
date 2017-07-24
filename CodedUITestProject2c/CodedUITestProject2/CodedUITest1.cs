d56c7be0711b6f7e46a68f1264f7a18bfb727846
Umer Farooq <k142229@nu.edu.pk>
2017-07-24 12:20:36 +0500

    Add files via upload

---
+++ CodedUITestProject2c/CodedUITestProject2/CodedUITest1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using CodedUITestProject2;
+using static CodedUITestProject2.UIMap;
+
+namespace CodedUITestProject2
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+
+            LoginCaseParam loginCaseParam = new LoginCaseParam            {
+
+                username="Test123",
+                password = "Pass123"
+
+            };
+
+            // this.UIMap.LoginCase("http://adactin.com/HotelApp/", "Arsalan","Abc123");
+            //this.UIMap.LoginCase("SamiTest123","Test123");
+           this.UIMap.LoginCase(loginCaseParam);
+
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod2()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
